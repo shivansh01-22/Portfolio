8e312987e87805f692e09b4ab62ad1be28be563e
shivansh01-22 <112016690+shivansh01-22@users.noreply.github.com>
2025-05-24 12:17:26 +0530

    chore: Updated resume content, changed project image and GitHub/live link

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,18 +1,4 @@
-/*!
-=========================================================
-* Meyawo Landing page
-=========================================================
 
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// smooth scroll
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
 
@@ -31,7 +17,6 @@ $(document).ready(function(){
     });
 });
 
-// navbar toggle
 $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
